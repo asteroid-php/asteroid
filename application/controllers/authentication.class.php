8ab73494e3f97b8b74e5d7e3b6507d3a549f22ac
Samuel Elliott <samuelthomas2774@users.noreply.github.com>
2016-05-24 21:11:36 +0100

    Updated authentication controller
    - Added custom scope
    - Added state checking
    - Added redirecting to the homepage, or a url specified in the next
    parameter
    - Fixed user being logged in on the logout page

--- application/controllers/authentication.class.php
+++ application/controllers/authentication.class.php
@@ -12,39 +12,57 @@
 		}
 		
 		public function login() {
+			// Save next url
+			if(is_string($next = $this->application->request()->query("next")))
+				$this->application->session()->set([ "auth", "next" ], $next);
+			else
+				$this->application->session()->set([ "auth", "next" ], null);
+			
 			// Redirect to the login dialog
 			$oauth = $this->application->authentication()->oauth();
-			$oauth->loginRedirect($this->application->generateURL($this->application->getControllerURL(), "code"), Array("user", "user:email"));
+			$oauth->loginRedirect($this->application->generateURL($this->application->getControllerURL(), "code"), $this->application->configuration([ "oauth", "client_scope" ]));
 		}
 		
 		public function code() {
 			$oauth = $this->application->authentication()->oauth();
 			if(is_string($code = $this->application->request()->query("code")) && is_string($state = $this->application->request()->query("state"))) {
 				try {
-					$oauth->getAccessTokenFromCode($this->application->generateURL($this->application->getControllerURL(), "code"), $code, false);
+					$oauth->getAccessTokenFromCode($this->application->generateURL($this->application->getControllerURL(), "code"), $code, $state);
 					$user = $oauth->userProfile();
 					if(!is_string($oauth->accessToken()) || !isset($user->id))
 						$this->application->error("Unknown error.")->view()->render("blank");
 					
-					$this->application->success("Logged in as {$user->name}.")->view()->render("blank");
+					$this->application->success("Logged in as {$user->name}.");
 				} catch(\Exception $error) {
 					//
-					$this->application->error($error->getMessage())->view()->render("blank");
+					$this->application->error($error->getMessage());
 				}
-			} elseif(is_string($error = $this->application->request()->query("error")) || true) {
-				switch($error) {
+			} else {
+				switch($this->application->request()->query("error")) {
 					default: $error = "Unknown error."; break;
 					case "access_denied": $error = "You canceled logging in."; break;
 				}
 				
-				$this->application->error($error)->view()->render("blank");
+				$this->application->error($error);
 			}
+			
+			// Redirect to next url or home
+			if(is_string($next = $this->application->session()->get([ "auth", "next" ]))) {
+				if($this->application->errors())
+					$this->application->message("Redirecting to **" . $next . "**");
+				
+				$this->application->events()->bind("render_body", function() use($next) {
+					return "<script data-ajaxify=\"\">$(document).ready(function() { var messages = []; $(\".messages > *\").each(function() { messages.push({ class: $(this).attr(\"class\"), html: $(this).html() }); }); History.replaceState({ messages: messages }, null, \"" . htmlentities($next) . "\"); });</script>";
+				});
+				
+				$this->application->view()->render("blank");
+			} else $this->application->redirectWithMessages("index");
 		}
 		
 		public function logout() {
 			// Delete the access token
 			if($this->application->authentication()->loggedin()) {
-				$this->application->authentication()->oauth()->session("token", null);
+				$this->application->authentication()->oauth()->accessToken(null);
 				$this->application->success("You have been logged out.")->view()->render("blank");
 			} else
 				// The user was never logged in!
