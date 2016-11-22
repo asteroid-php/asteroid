ae74cefdfbcf30c94246b24d83a78f003ee67645
Samuel Elliott <samuelthomas2774@users.noreply.github.com>
2016-11-22 20:48:42 +0000

    Updated autoloader
    - Added checking for files in normal case as well as lowercase

--- application/autoloader.class.php
+++ application/autoloader.class.php
@@ -11,7 +11,7 @@
 		public function __construct($namespace = null, $directory = null, $extension = null) {
 			if(is_string($directory)) {
 				if(file_exists($directory) && is_dir($directory)) $this->directory = rtrim($directory, "/");
-				else throw new Exception(__METHOD__ . "(): Directory must exist.");
+				else throw new Exception(__METHOD__, "Directory must exist.");
 			} else $this->directory = __DIR__;
 			if(is_string($extension)) $this->extension = $extension;
 			if(is_string($namespace)) $this->namespace = $namespace;
@@ -19,12 +19,15 @@
 		
 		public function load($class) {
 			if(!is_string($this->namespace)) $namespace = "";
-			else $namespace = strtolower(trim(str_replace("\\", "/", $this->namespace), "/")) . "/";
+			else $namespace = trim(str_replace("\\", "/", $this->namespace), "/") . "/";
 			if($namespace == "/") $namespace = "";
-			$class = preg_replace("/[^a-z0-9\/]/", "", strtolower(str_replace("\\", "/", $class)));
+			$class = preg_replace("/[^a-z0-9\/]/", "", str_replace("\\", "/", $class));
 			if(substr($class, 0, strlen($namespace)) != $namespace) return false;
 			$path = rtrim($this->directory, "/") . "/" . substr($class, strlen($namespace)) . $this->extension;
-			if(file_exists($path) && is_file($path))
+			
+			if(file_exists($path) && is_file($path) && is_readable($path))
+				require_once $path;
+			elseif(file_exists($path = strtolower($path)) && is_file($path) && is_readable($path))
 				require_once $path;
 		}
 		
