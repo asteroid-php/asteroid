af70fd1df49bd40618add88882d4f60e2dbc79bb
Samuel Elliott <samuelthomas2774@users.noreply.github.com>
2016-11-22 22:32:23 +0000

    Updated about controller
    - Added status objects
    - Added Facebook and Twitter redirects
    - Added terms of service and privacy policy pages
    - Contact form sends html

--- application/controllers/about.class.php
+++ application/controllers/about.class.php
@@ -8,40 +8,66 @@
 	use Asteroid\BaseController;
 	class About extends BaseController {
 		public function index() {
-			$this->application->view()->title("About")->render("about/about");
+			if(is_string($about = $this->configuration->get([ "about_file" ])) && $this->application->filesystem()->file($about))
+				$about = $this->application->filesystem()->read($about);
+			else $about = $this->configuration->get([ "about_text" ]);
+			
+			$class = $this->configuration->get([ "parsedown_class" ]);
+			$parsedown = new $class();
+			$parsedown->setBreaksEnabled(true);
+			
+			return $this->application->view()->title("About")->render("about/about", Array("about" => $about));
 		}
 		
 		public function contact($save = false) {
 			if($save == "save") {
 				$model = $this->application->model("About");
 				$data = $model->getAndValidateContactData($this->application->request()->post("name"), $this->application->request()->post("contact"), $this->application->request()->post("message"));
-				if($data->get([ "success" ]) != true)
-					$this->application->view()->renderJSON(Array("success" => false, "error" => $data->get([ "error" ])));
+				if($data->get([ "success" ]) !== true)
+					return $this->application->error($data->get([ "error" ]))->redirectWithMessages($this->application->getControllerURL(), "contact");
 				
 				list($name, $contact, $message) = $data->geta([ "name" ], [ "contact" ], [ "message" ]);
 				
-				$database = $this->application->database($this->application->configuration()->controller()->get([ "contact_database" ]));
-				$table = $this->application->configuration()->controller()->get([ "contact_table" ]);
-				$user_id = $this->application->authentication()->loggedin($user) == true ? $user->getID() : null;
+				$database = $this->application->database($this->configuration->get([ "contact_database" ]));
+				$table = $this->configuration->get([ "contact_table" ]);
+				$user_id = $this->application->authentication()->loggedin($user) === true ? $user->getID() : null;
 				$data = $model->saveContactData($database, $table, $user_id, $name, $contact, $message);
-				if($data->get([ "success" ]) != true)
-					$this->application->view()->renderJSON(Array("success" => false, "error" => $this->application->errors() ? implode(", ", $querydata->get([ "error" ])) : null));
+				if($data->get([ "success" ]) !== true)
+					return $this->application->error($this->application->errors() ? implode(", ", (array)$data->get([ "error" ])) : null)->error("Unknown error saving form response.")->redirectWithMessages($this->application->getControllerURL());
 				
 				$id = $data->get([ "id" ]);
 				
 				// Success!
-				// Send an email to $config->about->contact_addresses
-				$contact_addresses = $this->application->configuration()->controller()->get([ "contact_addresses" ]);
-				$contact_url = $this->application->generateURL($this->application->getControllerURL(), "contact");
+				// Send an email to contact_addresses
+				$contact_addresses = $this->configuration->get([ "contact_addresses" ]);
+				$contact_url = $this->application->generateURL($this->application->getControllerURL());
 				$controlpanel_url = $this->application->generateURL("control-panel");
-				$sent = $model->sendContactNotification($contact_addresses, $contact_url, $controlpanel_url, $name, $contact, $message, $id);
+				$mail = $model->sendContactNotification($contact_addresses, $contact_url, $controlpanel_url, $name, $contact, $message, $id);
 				
 				// Return success, even if the email couldn't send
-				$this->application->view()->renderJSON(Array("success" => true, "mailStatus" => $sent ? "Sent!" : "Error Sending: " . $mail->ErrorInfo));
-			} else {
-				// Show the about/contact view
-				$this->application->view()->title("Contact")->render("about/contact");
+				return $this->application->success("**Success!** Form response saved")->message($this->application->errors() ? ($mail === true ? "Sent notification email" : "**Error sending email:** " . $mail) : null)->redirectWithMessages($this->application->getControllerURL());
 			}
+			
+			// Show the about/contact view
+			return $this->application->view()->title("Contact")->render("about/contact");
+		}
+		
+		public function facebook() {
+			if(!is_string($url = $this->application->configuration()->controller()->get([ "facebook_url" ])))
+				return "_404";
+			
+			$ext_url = implode("/", func_get_args());
+			if(strlen($ext_url) > 0) $ext_url = "/" . $ext_url;
+			return $this->application->redirect("https://facebook.com/" . trim($url, "/") . $ext_url);
+		}
+		
+		public function twitter() {
+			if(!is_string($url = $this->application->configuration()->controller()->get([ "twitter_url" ])))
+				return "_404";
+			
+			$ext_url = implode("/", func_get_args());
+			if(strlen($ext_url) > 0) $ext_url = "/" . $ext_url;
+			return $this->application->redirect("https://twitter.com/" . trim($url, "/") . $ext_url);
 		}
 		
 		public function github() {
@@ -50,7 +76,35 @@
 			
 			$ext_url = implode("/", func_get_args());
 			if(strlen($ext_url) > 0) $ext_url = "/" . $ext_url;
-			$this->application->redirect("https://github.com/" . trim($url, "/") . $ext_url);
+			return $this->application->redirect("https://github.com/" . trim($url, "/") . $ext_url);
+		}
+		
+		public function terms() {
+			if(is_string($terms = $this->configuration->get([ "terms_file" ])) && $this->application->filesystem()->file($terms))
+				$this->configuration->set([ "terms_text" ], $this->application->filesystem()->read($terms));
+			
+			if(!is_string($terms = $this->configuration->get([ "terms_text" ])))
+				return "_404";
+			
+			$class = $this->configuration->get([ "parsedown_class" ]);
+			$parsedown = new $class();
+			$parsedown->setBreaksEnabled(true);
+			
+			return $this->application->view()->title("Terms of Service")->renderString("<h2>Terms of Service</h2>" . $parsedown->text($terms));
+		}
+		
+		public function privacy() {
+			if(is_string($privacy = $this->configuration->get([ "privacy_file" ])) && $this->application->filesystem()->file($privacy))
+				$this->configuration->set([ "privacy_text" ], $this->application->filesystem()->read($privacy));
+			
+			if(!is_string($privacy = $this->configuration->get([ "privacy_text" ])))
+				return "_404";
+			
+			$class = $this->configuration->get([ "parsedown_class" ]);
+			$parsedown = new $class();
+			$parsedown->setBreaksEnabled(true);
+			
+			return $this->application->view()->title("Privacy Policy")->renderString("<h2>Privacy Policy</h2>" . $parsedown->text($privacy));
 		}
 	}
 	
