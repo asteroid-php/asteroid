d08e83f8292c41e09009b93d9ffe279e76da16ab
Samuel Elliott <samuelthomas2774@users.noreply.github.com>
2016-11-22 21:59:18 +0000

    Updated authentication controller
    - Added status objects

--- application/controllers/authentication.class.php
+++ application/controllers/authentication.class.php
@@ -8,19 +8,20 @@
 	use Asteroid\BaseController;
 	class Authentication extends BaseController {
 		public function index() {
-			$this->application->redirect($this->application->generateURL("account"));
+			return $this->application->redirect($this->application->generateURL("account"));
 		}
 		
 		public function login() {
 			// Save next url
 			if(is_string($next = $this->application->request()->query("next")))
 				$this->application->session()->set([ "auth", "next" ], $next);
-			else
-				$this->application->session()->set([ "auth", "next" ], null);
+			else $this->application->session()->set([ "auth", "next" ], null);
 			
 			// Redirect to the login dialog
 			$oauth = $this->application->authentication()->oauth();
 			$oauth->loginRedirect($this->application->generateURL($this->application->getControllerURL(), "code"), $this->application->configuration([ "oauth", "client_scope" ]));
+			
+			return $this->application->status("Success");
 		}
 		
 		public function code() {
@@ -30,7 +31,7 @@
 					$oauth->getAccessTokenFromCode($this->application->generateURL($this->application->getControllerURL(), "code"), $code, $state);
 					$user = $oauth->userProfile();
 					if(!is_string($oauth->accessToken()) || !isset($user->id))
-						$this->application->error("Unknown error.")->view()->render("blank");
+						return $this->application->error("Unknown error.")->view()->render("blank");
 					
 					$this->application->success("Logged in as {$user->name}.");
 				} catch(\Exception $error) {
@@ -55,18 +56,18 @@
 					return "<script data-ajaxify=\"\">$(document).ready(function() { var messages = []; $(\".messages > *\").each(function() { messages.push({ class: $(this).attr(\"class\"), html: $(this).html() }); }); History.replaceState({ messages: messages }, null, \"" . htmlentities($next) . "\"); });</script>";
 				});
 				
-				$this->application->view()->render("blank");
-			} else $this->application->redirectWithMessages("index");
+				return $this->application->view()->render("blank");
+			} else return $this->application->redirectWithMessages("index");
 		}
 		
 		public function logout() {
 			// Delete the access token
 			if($this->application->authentication()->loggedin()) {
 				$this->application->authentication()->oauth()->accessToken(null);
-				$this->application->success("You have been logged out.")->view()->render("blank");
+				return $this->application->success("You have been logged out.")->view()->render("blank");
 			} else
 				// The user was never logged in!
-				$this->application->error("You are not logged in.")->view()->render("blank");
+				return $this->application->error("You are not logged in.")->view()->render("blank");
 		}
 	}
 	
